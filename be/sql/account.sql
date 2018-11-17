4e21e2b3e8252c428dfde71d59f190bc53062f74
sasharudenya <sasha.rudenya@yandex.ru>
2018-11-17 03:45:15 +0300

    sql

--- be/sql/account.sql
+++ be/sql/account.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `account` (
-  `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(45) DEFAULT NULL,
-  `first_name` varchar(45) DEFAULT NULL,
-  `registration_date` date DEFAULT NULL,
-  `image_profile` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`account_id`),
-  UNIQUE KEY `account_id_UNIQUE` (`account_id`),
-  CONSTRAINT `account_user_user_id_fk` FOREIGN KEY (`account_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1
+	`account_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`last_name` VARCHAR(255) NULL DEFAULT '0',
+	`first_name` VARCHAR(255) NULL DEFAULT '0',
+	`registration_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	`image_profile` VARCHAR(255) NULL DEFAULT NULL,
+	PRIMARY KEY (`account_id`),
+	UNIQUE INDEX `account_id` (`account_id`)
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
--- be/sql/user.sql
+++ be/sql/user.sql
@@ -1,9 +1,11 @@
-CREATE TABLE `user` (
-  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(45) NOT NULL DEFAULT 'user',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
-  UNIQUE KEY `login_UNIQUE` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=1
+CREATE TABLE `user`(
+	`user_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`login` VARCHAR(255) NOT NULL DEFAULT '0',
+	`password` VARCHAR(255) NOT NULL,
+	`role` VARCHAR(255) NOT NULL DEFAULT 'user',
+	PRIMARY KEY (`user_id`),
+	UNIQUE INDEX `user_id` (`user_id`),
+	UNIQUE INDEX `login` (`login`)
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB;
